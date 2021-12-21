6aba9c29365bcecbd79e028a96896a1a463933ea
Giovanni De Cesare <decesare.giovanni@gmail.com>
2021-12-21 16:59:06 +0100

    daylight circles of earth

--- script.js
+++ script.js
@@ -1,42 +1,51 @@
-console.clear()
+let
+  isHover = false,
+  frame = 0;
 
-new Vue({
-  el: '#js-ohgiri',
+const
+  MAX_ANG = 23.4,
+  MONTHS = ['December', 'January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November'];
+
+const
+  label = document.getElementById('label'),
+  northLabel = document.getElementById('northLabel'),
+  southLabel = document.getElementById('southLabel'),
+  angleCss = document.createElement('style');
+document.head.prepend(angleCss);
+
+function setAngle(angValue) {
+  let ang = easeAng(angValue);
+  angleCss.innerText =
+    `body {
+      --ang: ${ang};
+      --ang-full: ${angValue};
+      --ang-z: ${10 - Math.floor((angValue) * 10)};
+    }`;
   
-  mounted () {
-    for (let i = 0; i < 300; i++) {
-      // element
-      const snow = this.$refs['snow' + i]
+  label.innerHTML =
+    Math.abs(ang * MAX_ANG).toFixed(1) +
+    '&deg<br/>' +
+    MONTHS[Math.floor(((angValue * 12) + 0.7) % 12)];
+  
+  const hrs = Math.round(7 + (5 * (1 + ang)));
+  northLabel.innerText = hrs + ' hrs';
+  southLabel.innerText = (24 - hrs) + ' hrs';
+}
 
-      // 基準のx値
-      const baseX = gsap.utils.random(-10, 110)
-      
-      // 初期配置
-      gsap.set(snow, {
-        x: baseX + 'vw',
-        y: -10,
-        opacity: gsap.utils.random(0, 1),
-        scale: gsap.utils.random(0, 1)
-      })
-      
-      // 縦方向
-      gsap.to(snow, {
-        duration: gsap.utils.random(10, 30),
-        y: '100vh',
-        delay: gsap.utils.random(0, -30),
-        repeat: -1,
-        ease: 'none'
-      })
-      
-      // 横方向
-      gsap.to(snow, {
-        duration: gsap.utils.random(5, 15),
-        x: baseX + gsap.utils.random(-12, 12) + 'vw',
-        yoyo: true,
-        repeat: -1,
-        delay: gsap.utils.random(-20, -10),
-        ease: 'power1.inOut'
-      })
-    }
+function easeAng(angValue) {
+  let eased = (angValue > 0.5 ? 1 - angValue : angValue) * 2;
+  eased = -0.5 * (Math.cos(Math.PI * eased) - 1);
+  return (2 * eased) - 1;
+}
+
+function hoverOrbit(e) {
+  frame = 125 * (e.offsetX / e.target.offsetWidth);
+}
+
+setInterval(() => {
+  if (!isHover) {
+    frame += 1;
+    frame %= 250;
   }
-})
+  setAngle(frame / 250);
+}, 50);
