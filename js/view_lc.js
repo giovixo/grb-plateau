a69d77a1de2aee218e3eec853551b0e0664f854f
Giovanni De Cesare <decesare.giovanni@gmail.com>
2021-12-28 16:27:30 +0100

    caption added

--- js/view_lc.js
+++ js/view_lc.js
@@ -1,4 +1,4 @@
-function renderHello() {
+function plotData() {
 	// Template
 	var template = document.getElementById('template').innerHTML;
 	
@@ -6,7 +6,10 @@ function renderHello() {
 	var selectElement = document.querySelector('#select1');
 	var output = selectElement.value;
 	
+	document.getElementById('target').innerHTML = "Loading ...";
+	setTimeout (function () {		
 	// rendering
 	var rendered = Mustache.render(template, { name: output});
 	document.getElementById('target').innerHTML = rendered;
+	}, 500);
 }
